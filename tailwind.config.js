f0944b8368c7c0e5743bb3e33e1b88a7e0f09963
Folami Oluwapelumi <folamioluwapelumi1@gmail.com>
2022-07-01 12:53:30 -0700

    burgerwebpage

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'black': '#000000',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Outfit', 'serif'],
+    },
+    opacity: {
+      '5':'0.05',
+      '15': '0.15',
+      '35': '0.35',
+      '50':'0.50',
+      '65': '0.65',
+      '85': '0.85',
+      '100': '1',
+     },
+    extend: {},
+  },
+  plugins: [],
+}
